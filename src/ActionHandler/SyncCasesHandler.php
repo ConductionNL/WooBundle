43d7b733ad23336d771d8c50275f2eabc2675663
Barry Brands <barrybrands02@hotmail.com>
2023-08-14 16:48:08 +0200

    Sync cases action/handler/service added

---
+++ src/ActionHandler/SyncCasesHandler.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace CommonGateway\PDDBundle\ActionHandler;
+
+use CommonGateway\PDDBundle\Service\SyncCasesService;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+use CommonGateway\CoreBundle\ActionHandler\ActionHandlerInterface;
+
+class SyncPubHandler implements ActionHandlerInterface
+{
+    private SyncCasesService $service;
+
+    public function __construct(SyncCasesService $service)
+    {
+        $this->service = $service;
+    }
+
+    /**
+     *  This function returns the requered configuration as a [json-schema](https://json-schema.org/) array.
+     *
+     * @throws array a [json-schema](https://json-schema.org/) that this  action should comply to
+     */
+    public function getConfiguration(): array
+    {
+        return [
+            '$id'        => 'https://commongateway.nl/pdd.SyncCasesAction.action.json',
+            '$schema'    => 'https://docs.commongateway.nl/schemas/ActionHandler.schema.json',
+            'title'      => 'SyncCasesHandler',
+            'description'=> 'Handles the sync for xxllnc cases.',
+            'required'   => [],
+            'properties' => [],
+        ];
+    }
+
+    /**
+     * This function runs the SyncCases service plugin.
+     *
+     * @param array $data          The data from the call
+     * @param array $configuration The configuration of the action
+     *
+     * @throws TransportExceptionInterface|LoaderError|RuntimeError|SyntaxError
+     *
+     * @return array
+     */
+    public function run(array $data, array $configuration): array
+    {
+        return $this->service->syncCasesHandler($data, $configuration);
+    }
+}
---
+++ src/Service/SyncCasesService.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace CommonGateway\PDDBundle\Service;
+
+use App\Service\SynchronizationService;
+use CommonGateway\CoreBundle\Service\CallService;
+use CommonGateway\CoreBundle\Service\GatewayResourceService;
+use CommonGateway\CoreBundle\Service\MappingService;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Cache\CacheException;
+use Psr\Cache\InvalidArgumentException;
+use Symfony\Component\HttpFoundation\Response;
+
+class SyncCasesService
+{
+    private GatewayResourceService $resourceService;
+    private CallService $callService;
+    private SynchronizationService $syncService;
+    private array $data;
+    private array $configuration;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        GatewayResourceService $resourceService,
+        CallService $callService,
+        SynchronizationService $syncService,
+        MappingService $mappingService
+    ) {
+        $this->entityManager = $entityManager;
+        $this->resourceService = $resourceService;
+        $this->callService = $callService;
+        $this->syncService = $syncService;
+        $this->mappingService = $mappingService;
+    }
+
+    /**
+     * Handles the synchronization of xxllnc cases.
+     *
+     * @param array $data
+     * @param array $configuration
+     *
+     * @throws CacheException|InvalidArgumentException
+     *
+     * @return array
+     */
+    public function syncCasesHandler(array $data, array $configuration): array
+    {
+        $this->data = $data;
+        $this->configuration = $configuration;
+
+        $source = $this->resourceService->getSource('', 'common-gateway/pdd-bundle');
+        $schema = $this->resourceService->getSchema('https://commongateway.nl/pdd.openWOO.schema.json', 'common-gateway/pdd-bundle');
+        $mapping = $this->resourceService->getMapping('https://commongateway.nl/mapping/pdd.CaseToWooObject.mapping.json', 'common-gateway/pdd-bundle');
+
+        $sourceConfig = $source->getConfiguration();
+
+        $response = $this->callService->getAllResults(
+            $source,
+            '/cases',
+            $sourceConfig
+        );
+
+        $responseItems = [];
+        foreach ($response as $result) {
+            $synchronization = $this->syncService->findSyncBySource($source, $schema, $result['id']);
+            $synchronization->setMapping($mapping);
+            $synchronization = $this->syncService->synchronize($synchronization, $result);
+
+            $responseItems[] = $synchronization->getObject()->toArray();
+        }
+
+        $this->data['response'] = new Response(json_encode($responseItems), 200);
+
+        return $this->data;
+    }
+}
